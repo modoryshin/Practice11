1566a233e8133cd877517fb9d87fa9d38d45e414
Admin <Admin@ADMIN->
2018-06-12 17:42:28 +0500

    Добавьте файлы проекта.

---
+++ Practice11/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[][]codetable = new char[2][];
+            char[] m1 = new char[71] {'А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Э','Ю','Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', ' ', '-', ':', ';', '!', '?' };
+            codetable[0] = m1;
+            char[] m2 = new char[71] { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','_','|','$','~','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '*', '&', '%', '^' };
+            codetable[1] = m2;
+            int finish = 0;
+            do
+            {
+                Console.WriteLine("Enter your text in Russian with all numbers written in their letter forms.");
+                string text = Console.ReadLine();
+                char[] txt = text.ToCharArray();
+                char[] coded=new char[txt.Length];
+                int j = 0;
+                foreach(char x in txt)
+                {
+                    for(int i = 0; i < codetable[0].Length; i++)
+                    {
+                        if (codetable[0][i] == x)
+                        {
+                            coded[j] = codetable[1][i];
+                            j++;
+                        }
+                    }
+                }
+                j = 0;
+               char[] coded1 = new char[coded.Length];
+                foreach (char x in coded)
+                {
+                    for (int i = 0; i < codetable[1].Length; i++)
+                    {
+                        if (codetable[1][i] == x)
+                        {
+                            coded1[j] = codetable[0][i];
+                            j++;
+                        }
+                    }
+                }
+                Console.WriteLine("Here is coded version of your text: \t" );
+                foreach (char e in coded)
+                    Console.Write(e);
+                Console.WriteLine();
+                Console.WriteLine("Here is code decoded again for you to check: \t");
+                foreach (char e in coded1)
+                    Console.Write(e);
+                Console.WriteLine();
+                Console.WriteLine("If you wish to exit enter exit. Otherwise press any other button.");
+                string f = Console.ReadLine();
+                if (f == "exit"||f=="учше")
+                    finish++;
+            } while (finish == 0);
+        }
+    }
+}
---
+++ Practice11/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Practice11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Practice11")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("72100da5-4494-463f-9d5c-04191888b6ae")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
